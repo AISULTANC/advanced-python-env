09cadc357b589fdab1ac050bdb53be0627b68467
bibop <55452707+AISULTANC@users.noreply.github.com>
2025-12-24 15:27:43 +0600

    Add files via upload

--- week 2/task8.py
+++ week 2/task8.py
@@ -1,7 +1,25 @@
 s1 = input()
 s2 = input()
 
-if sorted(s1) == sorted(s2):
-    print("YES")
-else:
+if len(s1) != len(s2):
     print("NO")
+else:
+    count1 = {}
+    count2 = {}
+    
+    for char in s1:
+        if char in count1:
+            count1[char] += 1
+        else:
+            count1[char] = 1
+
+    for char in s2:
+        if char in count2:
+            count2[char] += 1
+        else:
+            count2[char] = 1
+
+    if count1 == count2:
+        print("YES")
+    else:
+        print("NO")
